dee44339576ff5d0964216380c9aefd198080e00
Sami Jaanu <jaanusa.sj@gmail.com>
2019-09-02 09:40:35 +0300

    first commit

---
+++ kauppa/src/main/java/projekti/kauppa/KauppaApplication.java
@@ -0,0 +1,13 @@
+package projekti.kauppa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KauppaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KauppaApplication.class, args);
+	}
+
+}
